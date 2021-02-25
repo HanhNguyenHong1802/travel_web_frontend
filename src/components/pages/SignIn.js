4de069690cc39a05e3777ee9d7e0baec8cc40bf8
hanhnguyenhong18022000 <64351493+hanhnguyenhong18022000@users.noreply.github.com>
2021-02-25 09:12:52 +0700

    update src
    update src

---
+++ src/components/pages/SignIn.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+
+const SignUp = () => {
+      return(
+        <form class="box" action="index.html" method="post">
+          <h1>Login</h1>
+            <input type="text" name="" placeholder="Username"/>
+            <input type="password" name="" placeholder="Password"/>
+            <input type="submit" name="" value="Signup"/>
+        </form>
+      );
+    
+  
+}
+
+export default SignUp;
